437081b89600d5776854b96542254a4fbe95fd0b
Huu Hoang <nguyenhuuhoang171@gmail.com>
2021-05-14 21:24:35 +0700

    getApi from Adafruit

--- front-end/WateringManager/src/screen/home/index.js
+++ front-end/WateringManager/src/screen/home/index.js
@@ -7,16 +7,64 @@ import{
 import {styles} from './style'
 import { AnimatedCircularProgress } from 'react-native-circular-progress';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import { loadOptions } from '@babel/core';
+
+
+async function getApi(mqtt_key,aio_key){
+    //https://io.adafruit.com/api/v2/hoangnh/feeds/soil-moisture/data.json?X-AIO-Key=aio_OcQG34N38YJv3DMr4KnQx3BADRMb
+    var linkAPI = `https://io.adafruit.com/api/v2/${mqtt_key}/data.json?X-AIO-Key=${aio_key}`;
+    try{
+        let response = await fetch(linkAPI);
+        let responseJson = await response.json();
+        return responseJson[0].value;
+    }catch(error){
+        console.error(`Error is : ${error}`);
+    }
+     
+};
 
 export default class index extends Component{
+    constructor (props){
+        super(props);
+        this.state = ({
+            moisture : 0,
+            temperature : 0
+        });
+    }
+
+    componentDidMount(){
+        this.loadMoisture();
+        this.loadTemp();
+    }
+
+    loadMoisture(){
+        getApi("hoangnh/feeds/soil-moisture","aio_OcQG34N38YJv3DMr4KnQx3BADRMb").then( (result) => {  
+            this.setState( {moisture : result} ); 
+        });
+    }
+
+    loadTemp(){
+        getApi("hoangnh/feeds/temperature","aio_OcQG34N38YJv3DMr4KnQx3BADRMb").then( (result) => {  
+            this.setState( {temperature : result} ); 
+        });
+    }
+
     render(){
+        //
+        //var apiMoisture = "";
+        // getApi().then( (result) => {apiMoisture = result;} );
+        // console.log(JSON.stringify(apiMoisture));
+        // var do_am = JSON.stringify(apiMoisture) ;
+        //var do_am = apiMoisture;
+        
+
         return(
             <ScrollView style={styles.container}>
                 <View style={styles.panel}>
                     <Text style={styles.title}> Máy bơm 1</Text>
                     <View style={styles.row}>
                         <Text style={styles.text}>Nhiệt độ: 
-                                <Text> 25</Text>
+                                <Text> {this.state.temperature}</Text>
                                 <MaterialCommunityIcons
                                     name="temperature-celsius"
                                     size={20}
@@ -47,7 +95,7 @@ export default class index extends Component{
                     </View>
                     <View style={styles.row}>
                         <Text style={styles.text}>Độ ẩm: 
-                                <Text> 75%</Text>
+                                <Text> {this.state.moisture} </Text>
                             </Text>
                         <AnimatedCircularProgress
                             size={120}
@@ -70,5 +118,7 @@ export default class index extends Component{
                 </View>
             </ScrollView>
         )
+        
     }
 }
+
