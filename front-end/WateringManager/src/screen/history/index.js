51a8116bcf26531b3f9b55b38a90e643b9f1a24c
Huu Hoang <nguyenhuuhoang171@gmail.com>
2021-06-12 16:43:36 +0700

    xong history

--- front-end/WateringManager/src/screen/history/index.js
+++ front-end/WateringManager/src/screen/history/index.js
@@ -10,22 +10,45 @@ import {Picker} from '@react-native-picker/picker';
 import FontAwesome from 'react-native-vector-icons/FontAwesome'
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
-import {getApi, postApi,getKey} from '../index.js'
+
 
 export default class index extends Component{
     state={
-        time:""
+        time:"",
+        data:[]
     }
+
+    componentDidMount(){
+        this.loadData();
+    }
+
+    loadData(){
+        getApi("hoangnh/feeds/soil-moisture", "aio_zpPc43KdQ2oo7bsUoxu4BpiL1cZo" ).then( (moisture_datas)=>{
+            getApi("hoangnh/feeds/temperature", "aio_zpPc43KdQ2oo7bsUoxu4BpiL1cZo" ).then((temp_datas)=>{
+                var _data = [];
+                for (let i = 0 ; i<4 ; i++){
+                    _data.push({    title : "Máy 1",
+                                    id: i,
+                                    time: String(moisture_datas[i].created_at).substring(11,16),
+                                    humidity : JSON.parse(moisture_datas[i].value).data,
+                                    temperature : String(JSON.parse(temp_datas[i].value).data).split('-')[0] ,
+                    });
+                }
+                this.setState({data : _data});
+            });           
+        });
+    }
+
     render(){
-        const data=[
-            {title:"Máy 1", temperature: "25", humidity:"75"},
-            {title:"Máy 2", temperature: "25", humidity:"75"},
-            {title:"Máy 3", temperature: "25", humidity:"75"}
-        ]
+        // const data=[
+        //     {title:"Máy 1", temperature: "25", humidity:"75"},
+        //     {title:"Máy 2", temperature: "25", humidity:"75"},
+        //     {title:"Máy 3", temperature: "25", humidity:"75"}
+        // ]
 
         return(
             <ScrollView style={styles.container}>
-                <View style={styles.picker}>
+                {/* <View style={styles.picker}>
                     <Picker
                         selectedValue={this.state.time}
                         onValueChange={(itemValue, itemIndex) =>
@@ -38,14 +61,22 @@ export default class index extends Component{
                         <Picker.Item label="8:00" value="8:00" />
                         <Picker.Item label="9:00" value="9:00" />
                     </Picker>
-                </View>
+                </View> */}
                 <View style={styles.panel}>
-                    {data.map(child=>(
+                    {this.state.data.map(child=>(
                         <TouchableOpacity
-                            key={child.title}
-                            onPress={()=>this.props.navigation.navigate('DetailHistory')}
+                            key={child.id}
+                            //onPress={()=>this.props.navigation.navigate('DetailHistory')}
                             style={styles.buttonContainer} >
                             <Text style={styles.title}> {child.title}</Text>
+                            <View style={styles.row}>
+                                <FontAwesome
+                                    name="clock-o"
+                                    size={25}
+                                    color="white"
+                                />
+                                <Text style={styles.text}>{child.time}</Text>
+                            </View>
                             <View style={styles.row}>
                                 <FontAwesome
                                     name="thermometer-2"
@@ -66,7 +97,7 @@ export default class index extends Component{
                                     size={25}
                                     color="blue"
                                 />
-                                <Text style={styles.text}>{child.humidity}%</Text>
+                                <Text style={styles.text}>{child.humidity}</Text>
                             </View>
                         </TouchableOpacity>
                     ))}
@@ -75,3 +106,16 @@ export default class index extends Component{
         )
     }
 }
+
+
+async function getApi(mqtt_key,aio_key){
+    var linkAPI = `https://io.adafruit.com/api/v2/${mqtt_key}/data.json?X-AIO-Key=${aio_key}`;
+    try{
+        let response = await fetch(linkAPI);
+        let responseJson = await response.json();
+        return responseJson;
+    }catch(error){
+        console.error(`Error is : ${error}`);
+    }
+     
+};
